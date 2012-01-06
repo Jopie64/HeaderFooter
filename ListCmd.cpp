863119ab6cface6ccea3e453e6b2ce9bcbe3a55e
Johan 't Hart <johanthart@gmail.com>
2012-01-06 23:17:06 +0100

    Now able to use --text option to view header and footer as text (not in hex)

--- ListCmd.cpp
+++ ListCmd.cpp
@@ -21,11 +21,20 @@ struct Ctxt
 	size_t headerSz;
 	size_t footerSz;
 	wstring dir;
+	bool headerAsText;
+	bool footerAsText;
 };
 
-void dumpStr(Ctxt& ctxt, istream& str, wostream& ostr, size_t length)
+void dumpStr(Ctxt& ctxt, istream& str, wostream& ostr, size_t length, bool asText)
 {
-	for(size_t i=0; i<length && str; ++i)
+	if(asText)
+	{
+		string text;
+		text.resize(length);
+		str.read(&*text.begin(),length);
+		ostr << JStd::String::ToWide(text, CP_ACP);
+	}
+	else for(size_t i=0; i<length && str; ++i)
 	{
 		char b = 0;
 		str.read(&b, 1);
@@ -43,13 +52,13 @@ void dumpHeaderFooter(Ctxt& ctxt, const wstring& file)
 		path = ctxt.dir.substr(0,slash + 1) + file;
 
 	ifstream fileStr(JStd::String::ToMult(path, CP_UTF8).c_str(), ios::binary | ios::in);
-	dumpStr(ctxt, fileStr, wcout, ctxt.headerSz);
+	dumpStr(ctxt, fileStr, wcout, ctxt.headerSz, ctxt.headerAsText);
 
 	wcout << "- ";
 	fileStr.clear();
 	fileStr.seekg(0, ios::end);
 	fileStr.seekg(fileStr.tellg() - (std::streamoff)ctxt.footerSz);
-	dumpStr(ctxt, fileStr, wcout, ctxt.footerSz);
+	dumpStr(ctxt, fileStr, wcout, ctxt.footerSz, ctxt.footerAsText);
 }
 
 void cmd(CmdLine& cmdLine)
@@ -57,9 +66,13 @@ void cmd(CmdLine& cmdLine)
 	wchar_t* dummy;
 
 	Ctxt ctxt;
-	ctxt.headerSz	= wcstol(cmdLine.next(L'h', L"hsize").c_str(), &dummy, 10);
-	ctxt.footerSz	= wcstol(cmdLine.next(L'f', L"fsize").c_str(), &dummy, 10);
-	ctxt.dir		= cmdLine.next();
+	ctxt.headerSz		= wcstol(cmdLine.next(L'h', L"hsize").c_str(), &dummy, 10);
+	ctxt.footerSz		= wcstol(cmdLine.next(L'f', L"fsize").c_str(), &dummy, 10);
+	ctxt.dir			= cmdLine.next();
+	ctxt.headerAsText	= cmdLine.hasOption(L"headerText");
+	ctxt.footerAsText	= cmdLine.hasOption(L"footerText");
+	if(cmdLine.hasOption(L't', L"text"))
+		ctxt.headerAsText = ctxt.footerAsText = true;
 
 	if(!ctxt.headerSz) ctxt.headerSz = 10;
 	if(!ctxt.footerSz) ctxt.footerSz = 10;
